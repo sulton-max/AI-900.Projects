bd87bca1406d1477bfb199b4349863554e841a39
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-02-07 17:49:10 +0500

    Add API configurations

---
+++ src/BikeUsers.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,65 @@
+namespace BikeUsers.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    /// <summary>
+    /// Registers NotificationDbContext in DI 
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddBikeUsageInfrastructure(this WebApplicationBuilder builder)
+    {
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures exposers including controllers
+    /// </summary>
+    /// <param name="builder">Application builder</param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures devTools including controllers
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Add Controller middleWhere
+    /// </summary>
+    /// <param name="app">Application host</param>
+    /// <returns>Application host</returns>
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+
+    /// <summary>
+    /// Add Controller middleWhere
+    /// </summary>
+    /// <param name="app">Application host</param>
+    /// <returns>Application host</returns>
+    private static WebApplication UseDevTools(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+}
